f4d0f282d3d98e3041e10f808344e49dc7a47295
Christian Bergmann <christian.bergmann@email.de>
2025-05-03 01:35:54 +0200

    Meine Termine App: Kalender-UI und Funktionen eingefügt

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,251 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MeineTermineApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MeineTermineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Meine Termine',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TermineHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TermineHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TermineHomePageState createState() => _TermineHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TermineHomePageState extends State<TermineHomePage> {
+  List<Map<String, dynamic>> termine = [];
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _selectedDay = DateTime.now();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    final initializationSettings = InitializationSettings(
+      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      iOS: DarwinInitializationSettings(),
+    );
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meine Termine'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "Liste"),
+              Tab(text: "Kalender"),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () async {
+                final neuerTermin = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NeuerTerminSeite(),
+                  ),
+                );
+                if (neuerTermin != null) {
+                  setState(() {
+                    termine.add(neuerTermin);
+                  });
+                  _scheduleMultipleNotifications(
+                    titel: neuerTermin['titel'],
+                    datum: neuerTermin['datum'],
+                  );
+                }
+              },
             ),
           ],
         ),
+        body: TabBarView(
+          children: [
+            _buildTerminListe(),
+            _buildKalender(),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildTerminListe() {
+    return ListView.builder(
+      itemCount: termine.length,
+      itemBuilder: (context, index) {
+        final termin = termine[index];
+        return ListTile(
+          leading: Icon(Icons.event, color: termin['farbe']),
+          title: Text(termin['titel']),
+          subtitle: Text(termin['datum'].toString()),
+        );
+      },
+    );
+  }
+
+  Widget _buildKalender() {
+    return TableCalendar(
+      focusedDay: _selectedDay,
+      firstDay: DateTime.utc(2000, 1, 1),
+      lastDay: DateTime.utc(2100, 12, 31),
+      calendarFormat: _calendarFormat,
+      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDay = selectedDay;
+        });
+      },
+      onFormatChanged: (format) {
+        setState(() {
+          _calendarFormat = format;
+        });
+      },
+      calendarStyle: CalendarStyle(
+        weekendTextStyle: TextStyle(color: Colors.red),
+      ),
+      headerStyle: HeaderStyle(
+        formatButtonVisible: true,
+        titleCentered: true,
+      ),
+    );
+  }
+
+  void _scheduleMultipleNotifications({required String titel, required DateTime datum}) async {
+    await _scheduleSingleNotification(
+      id: datum.hashCode,
+      titel: titel,
+      body: 'In 1 Woche: $titel',
+      scheduledTime: datum.subtract(Duration(days: 7)),
+    );
+
+    await _scheduleSingleNotification(
+      id: datum.hashCode + 1,
+      titel: titel,
+      body: 'In 1 Tag: $titel',
+      scheduledTime: datum.subtract(Duration(days: 1)),
+    );
+
+    await _scheduleSingleNotification(
+      id: datum.hashCode + 2,
+      titel: titel,
+      body: 'In 1 Stunde: $titel',
+      scheduledTime: datum.subtract(Duration(hours: 1)),
+    );
+
+    await _scheduleSingleNotification(
+      id: datum.hashCode + 3,
+      titel: titel,
+      body: 'In 10 Minuten: $titel',
+      scheduledTime: datum.subtract(Duration(minutes: 10)),
+    );
+  }
+
+  Future<void> _scheduleSingleNotification({
+    required int id,
+    required String titel,
+    required String body,
+    required DateTime scheduledTime,
+  }) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'deine_kalender_app',
+      'Terminerinnerungen',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: true,
+    );
+
+    var notificationDetails = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      titel,
+      body,
+      scheduledTime,
+      notificationDetails,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.dateAndTime,
+    );
+  }
+}
+
+class NeuerTerminSeite extends StatefulWidget {
+  @override
+  _NeuerTerminSeiteState createState() => _NeuerTerminSeiteState();
+}
+
+class _NeuerTerminSeiteState extends State<NeuerTerminSeite> {
+  final _titelController = TextEditingController();
+  DateTime? _ausgewaehltesDatum;
+  Color _kategorieFarbe = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Neuer Termin')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titelController,
+              decoration: InputDecoration(labelText: 'Titel'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                final datum = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                );
+                if (datum != null) {
+                  setState(() {
+                    _ausgewaehltesDatum = datum;
+                  });
+                }
+              },
+              child: Text(_ausgewaehltesDatum == null
+                  ? 'Datum auswählen'
+                  : _ausgewaehltesDatum.toString()),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                if (_titelController.text.isNotEmpty && _ausgewaehltesDatum != null) {
+                  Navigator.pop(context, {
+                    'titel': _titelController.text,
+                    'datum': _ausgewaehltesDatum,
+                    'farbe': _kategorieFarbe,
+                  });
+                }
+              },
+              child: Text('Termin speichern'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
