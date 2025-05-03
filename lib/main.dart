2732c76669be4bfce01ed9b429ef3834000747bc
Christian Bergmann <christian.bergmann@email.de>
2025-05-03 09:45:15 +0200

    endgültig perfekt korrigiert final

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.d
+  final List<String> _kategorien = ['Privat', 'Kunde', 'Lieferant'];
+  final List<String> _typen = ['Termin intern', 'Termin extern'];
+  String? _ausgewaehlteKategorie;
+  String? _ausgewaehlterTyp;
+art';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
