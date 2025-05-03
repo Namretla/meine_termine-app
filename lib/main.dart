326afbcaf60ac4d4f55d5e2bf1d70e50147bfb6c
Christian Bergmann <christian.bergmann@email.de>
2025-05-03 02:38:18 +0200

    Fix: NotificationDetails-Import und API-Version korrigiert

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/src/platform_specifics/android/notification_details.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
